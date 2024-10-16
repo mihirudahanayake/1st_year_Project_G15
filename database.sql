4f1e62be4b0eb1555a52293706362063ab21ee6e
mihirudahanayake <mihirudahanayake@gmail.com>
2024-10-16 22:46:56 +0530

    Add files via upload

--- database.sql
+++ database.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Aug 13, 2024 at 10:55 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- දායකයා: 127.0.0.1
+-- උත්පාදන වේලාව: ඔක්තෝම්බර් 16, 2024 දින 07:15 PM ට
+-- සේවාදායකයේ අනුවාදය: 10.4.32-MariaDB
+-- PHP අනුවාදය: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `database`
+-- දත්තගබඩාව: `database`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bookings`
+-- වගුවක් සඳහා වගු සැකිල්ල `bookings`
 --
 
 CREATE TABLE `bookings` (
@@ -37,16 +37,17 @@ CREATE TABLE `bookings` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `bookings`
+-- වගු සඳහා නික්ෂේප දත්ත `bookings`
 --
 
 INSERT INTO `bookings` (`booking_id`, `user_id`, `room_id`, `start_date`, `end_date`, `booking_status`) VALUES
-(6, 7, 12, '2024-08-13', '2024-08-14', 'confirmed');
+(6, 7, 12, '2024-08-13', '2024-08-14', 'confirmed'),
+(7, 10, 14, '2024-09-28', '2024-09-29', 'confirmed');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cities`
+-- වගුවක් සඳහා වගු සැකිල්ල `cities`
 --
 
 CREATE TABLE `cities` (
@@ -55,7 +56,7 @@ CREATE TABLE `cities` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `cities`
+-- වගු සඳහා නික්ෂේප දත්ත `cities`
 --
 
 INSERT INTO `cities` (`city_id`, `city_name`) VALUES
@@ -63,12 +64,13 @@ INSERT INTO `cities` (`city_id`, `city_name`) VALUES
 (11, 'matara'),
 (12, 'a'),
 (13, 'tan'),
-(999, 'New City Name');
+(999, 'New City Name'),
+(1000, 'rjt');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `destinations`
+-- වගුවක් සඳහා වගු සැකිල්ල `destinations`
 --
 
 CREATE TABLE `destinations` (
@@ -80,7 +82,7 @@ CREATE TABLE `destinations` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `destinations`
+-- වගු සඳහා නික්ෂේප දත්ත `destinations`
 --
 
 INSERT INTO `destinations` (`destination_id`, `desti_name`, `desti_description`, `image_url`, `city`) VALUES
@@ -92,12 +94,39 @@ INSERT INTO `destinations` (`destination_id`, `desti_name`, `desti_description`,
 (34, 'ryi', 'fyu', './uploads/search_9545869.png', 'Beliatta'),
 (36, 'asd', 'ds', './uploads/search.png', 'Beliatta'),
 (37, 'sdac', 'dsa', './uploads/search (1).png', 'Beliatta'),
-(38, 'fs', 'ss\r\nsdf\r\nsd\r\n\r\nd', './uploads/search (2).png', 'matara');
+(38, 'fs', 'ss\r\nsdf\r\nsd\r\n\r\nd', './uploads/search (2).png', 'matara'),
+(40, 'ff', 'gf', '', 'matara'),
+(42, 'rjt', 'hi', '', 'rjt');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `hotels`
+-- වගුවක් සඳහා වගු සැකිල්ල `destination_images`
+--
+
+CREATE TABLE `destination_images` (
+  `id` int(11) NOT NULL,
+  `destination_id` int(11) NOT NULL,
+  `image_url` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- වගු සඳහා නික්ෂේප දත්ත `destination_images`
+--
+
+INSERT INTO `destination_images` (`id`, `destination_id`, `image_url`) VALUES
+(1, 40, './uploads/rata_anurata_240826.jpg'),
+(2, 40, './uploads/OIP (5).jpeg'),
+(4, 17, './uploads/search (2).png'),
+(5, 18, './uploads/search (1).png'),
+(6, 19, './uploads/search_locate_find_icon-icons.com_67287.png'),
+(7, 42, './uploads/0e9e7802690c2c01050fc2c01317ee87.jpg'),
+(8, 42, './uploads/3655706b92e2321348b29e2a3e473900.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- වගුවක් සඳහා වගු සැකිල්ල `hotels`
 --
 
 CREATE TABLE `hotels` (
@@ -111,19 +140,17 @@ CREATE TABLE `hotels` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `hotels`
+-- වගු සඳහා නික්ෂේප දත්ත `hotels`
 --
 
 INSERT INTO `hotels` (`hotel_id`, `hotel_name`, `total_rooms`, `user_id`, `description`, `location`, `city_id`) VALUES
-(16, 'ww', 3, 7, 'ss', 'matara', 11),
-(18, 'eett', 3, 8, 'dd', 'Beliatta', 10),
-(30, 'abab', 2, 14, 'ab', 'a', 12),
-(31, 'New Hotel', 2, 11, 'hi', 'matara', 11);
+(16, 'ww', 1, 7, 'ss', 'tan', 11),
+(31, 'New Hotel', 5, 11, 'hi', 'matara', 11);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `hotel_destinations`
+-- වගුවක් සඳහා වගු සැකිල්ල `hotel_destinations`
 --
 
 CREATE TABLE `hotel_destinations` (
@@ -133,17 +160,37 @@ CREATE TABLE `hotel_destinations` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `hotel_destinations`
+-- වගු සඳහා නික්ෂේප දත්ත `hotel_destinations`
 --
 
 INSERT INTO `hotel_destinations` (`id`, `hotel_id`, `destination_id`) VALUES
 (4, 16, 3),
-(5, 16, 38);
+(5, 16, 38),
+(6, 31, 18);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `notifications`
+-- වගුවක් සඳහා වගු සැකිල්ල `hotel_images`
+--
+
+CREATE TABLE `hotel_images` (
+  `image_id` int(11) NOT NULL,
+  `hotel_id` int(11) DEFAULT NULL,
+  `image_path` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- වගු සඳහා නික්ෂේප දත්ත `hotel_images`
+--
+
+INSERT INTO `hotel_images` (`image_id`, `hotel_id`, `image_path`) VALUES
+(7, 31, 'uploads/hotel_images/Horizon_Club_Ocean_View_Room-1.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- වගුවක් සඳහා වගු සැකිල්ල `notifications`
 --
 
 CREATE TABLE `notifications` (
@@ -156,16 +203,17 @@ CREATE TABLE `notifications` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `notifications`
+-- වගු සඳහා නික්ෂේප දත්ත `notifications`
 --
 
 INSERT INTO `notifications` (`notification_id`, `hotel_id`, `room_id`, `message`, `is_read`, `created_at`) VALUES
-(4, 16, 12, 'Room 12 has been booked from 2024-08-13 to 2024-08-14.', 0, '2024-08-12 20:19:09');
+(4, 16, 12, 'Room 12 has been booked from 2024-08-13 to 2024-08-14.', 0, '2024-08-12 20:19:09'),
+(5, 31, 14, 'Room 14 has been booked from 2024-09-28 to 2024-09-29.', 0, '2024-09-28 09:44:01');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `rooms`
+-- වගුවක් සඳහා වගු සැකිල්ල `rooms`
 --
 
 CREATE TABLE `rooms` (
@@ -173,7 +221,6 @@ CREATE TABLE `rooms` (
   `hotel_id` int(11) DEFAULT NULL,
   `room_name` varchar(255) NOT NULL,
   `room_number` int(255) NOT NULL,
-  `room_description` text DEFAULT NULL,
   `price_per_night` decimal(10,2) NOT NULL,
   `max_adults` int(11) NOT NULL,
   `max_children` int(11) NOT NULL,
@@ -183,16 +230,91 @@ CREATE TABLE `rooms` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `rooms`
+-- වගු සඳහා නික්ෂේප දත්ත `rooms`
 --
 
-INSERT INTO `rooms` (`room_id`, `hotel_id`, `room_name`, `room_number`, `room_description`, `price_per_night`, `max_adults`, `max_children`, `availability`, `hotel_name`, `facilities`) VALUES
-(12, 16, '2e', 0, NULL, 7.00, 2, 1, 'e', NULL, '0');
+INSERT INTO `rooms` (`room_id`, `hotel_id`, `room_name`, `room_number`, `price_per_night`, `max_adults`, `max_children`, `availability`, `hotel_name`, `facilities`) VALUES
+(12, 16, '2e', 5, 7.00, 2, 1, 'available', NULL, '0'),
+(14, 31, 'nn', 7, 10.00, 2, 4, 'available', NULL, 'ac'),
+(15, 31, 'we', 27, 20.00, 2, 2, 'available', NULL, '0'),
+(27, 31, '', 3, 3.00, 2, 1, 'Available', NULL, 'ac'),
+(28, 31, '', 45, 4.00, 2, 1, 'Available', NULL, 'no'),
+(29, 31, '', 4, 2.00, 1, 0, 'Available', NULL, '2');
+
+--
+-- ප්‍රේරක `rooms`
+--
+DELIMITER $$
+CREATE TRIGGER `update_total_rooms_after_delete` AFTER DELETE ON `rooms` FOR EACH ROW BEGIN
+    -- Update the total_rooms in hotels table
+    UPDATE hotels
+    SET total_rooms = total_rooms - 1
+    WHERE hotel_id = OLD.hotel_id;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `update_total_rooms_after_insert` AFTER INSERT ON `rooms` FOR EACH ROW BEGIN
+    -- Update the total_rooms in hotels table
+    UPDATE hotels
+    SET total_rooms = total_rooms + 1
+    WHERE hotel_id = NEW.hotel_id;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `update_total_rooms_after_update` AFTER UPDATE ON `rooms` FOR EACH ROW BEGIN
+    -- If the hotel_id is changed, update the total rooms for both hotels
+    IF OLD.hotel_id != NEW.hotel_id THEN
+        -- Decrement from the old hotel
+        UPDATE hotels
+        SET total_rooms = total_rooms - 1
+        WHERE hotel_id = OLD.hotel_id;
+        
+        -- Increment for the new hotel
+        UPDATE hotels
+        SET total_rooms = total_rooms + 1
+        WHERE hotel_id = NEW.hotel_id;
+    END IF;
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- වගුවක් සඳහා වගු සැකිල්ල `room_images`
+--
+
+CREATE TABLE `room_images` (
+  `image_id` int(11) NOT NULL,
+  `room_id` int(11) DEFAULT NULL,
+  `image_path` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- වගු සඳහා නික්ෂේප දත්ත `room_images`
+--
+
+INSERT INTO `room_images` (`image_id`, `room_id`, `image_path`) VALUES
+(2, NULL, 'uploads/Screenshot 2024-08-13 152159.png'),
+(3, NULL, 'uploads/Screenshot 2024-08-13 152159.png'),
+(5, NULL, 'uploads/Screenshot 2024-08-16 220034.png'),
+(6, 14, 'uploads/r1iii.jpeg'),
+(7, 14, 'uploads/r1.jpg'),
+(28, 14, 'uploads/r1i.jpeg'),
+(30, 14, 'uploads/r1ii.jpeg'),
+(31, 27, 'room_27_66e514d86875f9.91549764.jpg'),
+(33, 15, 'uploads/R (2).jpeg'),
+(36, 15, 'uploads/room_27_image4.jpeg'),
+(37, 15, 'uploads/room_27_image3.jpeg'),
+(38, NULL, 'uploads/rooms/room__image1.jpg'),
+(39, 29, 'uploads/rooms/room_4_image1.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- වගුවක් සඳහා වගු සැකිල්ල `users`
 --
 
 CREATE TABLE `users` (
@@ -203,22 +325,27 @@ CREATE TABLE `users` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `user_type` varchar(20) NOT NULL,
   `description` varchar(20) NOT NULL,
-  `hotel_id` int(11) DEFAULT NULL
+  `hotel_id` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `telephone` varchar(15) DEFAULT NULL,
+  `profile_picture` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `users`
+-- වගු සඳහා නික්ෂේප දත්ත `users`
 --
 
-INSERT INTO `users` (`user_id`, `username`, `email`, `password`, `created_at`, `user_type`, `description`, `hotel_id`) VALUES
-(4, 'admin', 'admin@gmail.com', '$2y$10$aStoGgVpL8RqnYGZiqXmTu2uu3LAY2Lp0hs/57X944JZPnSVUeMrq', '2024-08-10 08:40:15', 'hotel_admin', '', 10),
-(7, 'er', 'ee@d', '$2y$10$ffv51s/rQSdKf3S3Q9IOVOsHIvbORo0Bho7qbL3zN4HnX3DrAlUSC', '2024-08-12 07:15:18', 'hotel_admin', '', 16),
-(8, 'rr', 'rr@r', '$2y$10$aT6w38RUqaZMaIUCMI/me.JDpiJhjMs0zBf0XFptTHXAhNlimvAHy', '2024-08-12 07:22:13', 'hotel_admin', '', 19),
-(9, 'Mihiru', 'mihiru09@gmail.com', '$2y$10$zHLJ7gUmy/YtJw87uxzTB.tEQMAc0J8MXGpD0rtWPwTKHqiVn9EwS', '2024-08-12 08:06:15', 'admin', '', NULL),
-(10, 'testuser', 'testuser@gmail.com', '$2y$10$W4EiVnim6hLPdTR6wE.Q2OLEK2ryF3c8uC1gmyb5A/UNiXcQGGA2e', '2024-08-13 07:26:15', 'user', '', NULL),
-(11, 'testhadmin', 'testhadmin@gmail.com', '$2y$10$G8wtC7bMPxKIIlkb4NXLGOXSFhGoXJGw7Mu.aaw/alYOnh3s9JC0W', '2024-08-13 07:27:26', 'hotel_admin', '', 31),
-(12, 'testadmin', 'testadmin@gmail.com', '$2y$10$MVdZIAk0vPDbTmgrsJFW/O1kX9sjgPB8.Brl6oiiiJM/TmxpOcjAy', '2024-08-13 07:28:12', 'admin', '', NULL),
-(14, 'ab', 'ab@a', '$2y$10$NEIrqCP20erFUxuh8yQdD.TEQZzi5wUwxq3/BA05l7S4Sxs6s1jXC', '2024-08-13 08:35:19', 'hotel_admin', '', 30);
+INSERT INTO `users` (`user_id`, `username`, `email`, `password`, `created_at`, `user_type`, `description`, `hotel_id`, `name`, `telephone`, `profile_picture`) VALUES
+(7, 'er', 'erer@d', '$2y$10$qPQFWjDYPAAOpKE.1.665ePJne03zdHARDK8MzOjmlVjjc2tXH1NO', '2024-08-12 07:15:18', 'hotel_admin', '', 16, NULL, NULL, NULL),
+(8, 'rr', 'rr@r', '$2y$10$aT6w3', '2024-08-12 07:22:13', 'user', '', 19, NULL, NULL, NULL),
+(9, 'Mihiru', 'mihiru09@gmail.com', '$2y$10$zHLJ7', '2024-08-12 08:06:15', 'admin', '', NULL, NULL, NULL, NULL),
+(10, 'testuser', 'testuser@gmail.com', '$2y$10$b5JXTAYnKnVOMJN6rstbt.oKb.vqtks4eS1fl38g71Ml5aK68kdcC', '2024-08-13 07:26:15', 'user', '', NULL, 'user', '0701234567', '1.png'),
+(11, 'testhadmin', 'testhadmin@gmail.com', '$2y$10$azimQGMjUJrViRdmD/3z0ur8Hp5X0BZKy9hvLfhVZCPCVIIe6U0N2', '2024-08-13 07:27:26', 'hotel_admin', '', 31, NULL, NULL, NULL),
+(12, 'testadmin', 'testadmin@gmail.com', '$2y$10$DByNtJngGCUTNKEV9itPaOQaPeMSbzpyFKVZHEbz.98CiDo9PP0R.', '2024-08-13 07:28:12', 'admin', '', NULL, NULL, NULL, NULL),
+(14, 'ab', 'ab@a', '$2y$10$NEIrq', '2024-08-13 08:35:19', 'hotel_admin', '', 30, NULL, NULL, NULL),
+(15, 'hadmin', 'ff@g', '$2y$10$x77Ls', '2024-10-16 08:15:24', 'user', '', NULL, NULL, NULL, NULL),
+(16, 'mm', 'mm@m', '$2y$10$dq.Yh', '2024-10-16 08:16:50', 'user', '', NULL, NULL, NULL, NULL),
+(17, 'tt', 'tt@t', '$2y$10$qPQFWjDYPAAOpKE.1.665ePJne03zdHARDK8MzOjmlVjjc2tXH1NO', '2024-10-16 08:19:02', 'user', '', NULL, NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -248,6 +375,13 @@ ALTER TABLE `destinations`
   ADD KEY `fk_city` (`city`);
 
 --
+-- Indexes for table `destination_images`
+--
+ALTER TABLE `destination_images`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `destination_id` (`destination_id`);
+
+--
 -- Indexes for table `hotels`
 --
 ALTER TABLE `hotels`
@@ -266,6 +400,13 @@ ALTER TABLE `hotel_destinations`
   ADD KEY `destination_id` (`destination_id`);
 
 --
+-- Indexes for table `hotel_images`
+--
+ALTER TABLE `hotel_images`
+  ADD PRIMARY KEY (`image_id`),
+  ADD KEY `hotel_id` (`hotel_id`);
+
+--
 -- Indexes for table `notifications`
 --
 ALTER TABLE `notifications`
@@ -282,10 +423,18 @@ ALTER TABLE `rooms`
   ADD KEY `fk_hotel_name` (`hotel_name`);
 
 --
+-- Indexes for table `room_images`
+--
+ALTER TABLE `room_images`
+  ADD PRIMARY KEY (`image_id`),
+  ADD KEY `room_id` (`room_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -295,19 +444,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `cities`
 --
 ALTER TABLE `cities`
-  MODIFY `city_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1000;
+  MODIFY `city_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1001;
 
 --
 -- AUTO_INCREMENT for table `destinations`
 --
 ALTER TABLE `destinations`
-  MODIFY `destination_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `destination_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
+-- AUTO_INCREMENT for table `destination_images`
+--
+ALTER TABLE `destination_images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `hotels`
@@ -319,45 +474,63 @@ ALTER TABLE `hotels`
 -- AUTO_INCREMENT for table `hotel_destinations`
 --
 ALTER TABLE `hotel_destinations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `hotel_images`
+--
+ALTER TABLE `hotel_images`
+  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `notifications`
 --
 ALTER TABLE `notifications`
-  MODIFY `notification_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `notification_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `rooms`
 --
 ALTER TABLE `rooms`
-  MODIFY `room_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `room_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- AUTO_INCREMENT for table `room_images`
+--
+ALTER TABLE `room_images`
+  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
--- Constraints for dumped tables
+-- නික්ෂේපනය කරන ලද වගු සඳහා සීමා බාධක
 --
 
 --
--- Constraints for table `bookings`
+-- වගුව සඳහා සීමා බාධක `bookings`
 --
 ALTER TABLE `bookings`
   ADD CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
   ADD CONSTRAINT `bookings_ibfk_2` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`room_id`) ON DELETE CASCADE;
 
 --
--- Constraints for table `destinations`
+-- වගුව සඳහා සීමා බාධක `destinations`
 --
 ALTER TABLE `destinations`
   ADD CONSTRAINT `fk_city` FOREIGN KEY (`city`) REFERENCES `cities` (`city_name`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `hotels`
+-- වගුව සඳහා සීමා බාධක `destination_images`
+--
+ALTER TABLE `destination_images`
+  ADD CONSTRAINT `destination_images_ibfk_1` FOREIGN KEY (`destination_id`) REFERENCES `destinations` (`destination_id`) ON DELETE CASCADE;
+
+--
+-- වගුව සඳහා සීමා බාධක `hotels`
 --
 ALTER TABLE `hotels`
   ADD CONSTRAINT `fk_hotel_city_id` FOREIGN KEY (`city_id`) REFERENCES `cities` (`city_id`),
@@ -365,25 +538,37 @@ ALTER TABLE `hotels`
   ADD CONSTRAINT `hotels_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 --
--- Constraints for table `hotel_destinations`
+-- වගුව සඳහා සීමා බාධක `hotel_destinations`
 --
 ALTER TABLE `hotel_destinations`
   ADD CONSTRAINT `hotel_destinations_ibfk_1` FOREIGN KEY (`hotel_id`) REFERENCES `hotels` (`hotel_id`) ON DELETE CASCADE,
   ADD CONSTRAINT `hotel_destinations_ibfk_2` FOREIGN KEY (`destination_id`) REFERENCES `destinations` (`destination_id`) ON DELETE CASCADE;
 
 --
--- Constraints for table `notifications`
+-- වගුව සඳහා සීමා බාධක `hotel_images`
+--
+ALTER TABLE `hotel_images`
+  ADD CONSTRAINT `hotel_images_ibfk_1` FOREIGN KEY (`hotel_id`) REFERENCES `hotels` (`hotel_id`) ON DELETE CASCADE;
+
+--
+-- වගුව සඳහා සීමා බාධක `notifications`
 --
 ALTER TABLE `notifications`
   ADD CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`hotel_id`) REFERENCES `hotels` (`hotel_id`),
   ADD CONSTRAINT `notifications_ibfk_2` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`room_id`);
 
 --
--- Constraints for table `rooms`
+-- වගුව සඳහා සීමා බාධක `rooms`
 --
 ALTER TABLE `rooms`
   ADD CONSTRAINT `fk_hotel_name` FOREIGN KEY (`hotel_name`) REFERENCES `hotels` (`hotel_name`) ON DELETE CASCADE,
   ADD CONSTRAINT `rooms_ibfk_1` FOREIGN KEY (`hotel_id`) REFERENCES `hotels` (`hotel_id`) ON DELETE CASCADE;
+
+--
+-- වගුව සඳහා සීමා බාධක `room_images`
+--
+ALTER TABLE `room_images`
+  ADD CONSTRAINT `room_images_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`room_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
